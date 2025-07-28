01c656dbee676b17816b03615db2357d6f3606d6
Ryan Small <rjsmall90@gmail.com>
2025-07-28 11:10:20 -0400

    commit b4 IDE update

---
+++ ui/src/components/skills.ts
@@ -0,0 +1,4 @@
+
+export default function skills(props: any) {
+
+}
--- ui/src/pages/routes/about.tsx
+++ ui/src/pages/routes/about.tsx
@@ -1,5 +1,14 @@
 
 
-export const About = () => {
+const About = () => {
+    return (
+        <div>
+            <div className="about-me">
+                <p> Experienced software engineer and community leader with a strong foundation in user experience, stakeholder communication, and scalable system design. Proven success in leading technical migrations, designing accessible solutions, and delivering impactful digital experiences. Adept at collaborating across teams to drive transformation and solve complex problems. Now seeking to apply these skills in strategy- focused roles within finance, consulting, or innovation-led organizations. </p>
+            </div>
+        </div>
+    )
 
 }
+
+export default About
