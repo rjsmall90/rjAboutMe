270d14671f70df36773c75b7c9cf962dee326b3f
Ryan Small <rjsmall90@gmail.com>
2025-08-05 01:08:59 -0400

    completed generated quotes component

--- server/index.js
+++ server/index.js
@@ -1,24 +1,13 @@
 import express from 'express'
-import  { fetchQuotes } from './data/quotes'
+import  { fetchQuotes } from './data/quotes.js'
 
 const server = express()
 const port = 8000
 
-server.get('/quotes', function(req, res) {
-    const response = fetchQuotes(req, res)
+server.get('/quotes', async function(req, res) {
+    const response = await fetchQuotes(req, res)
     res.send(response)
 })
-
-// server.get('/quotes', async function (req,res) {
-//     let response
-//     try {
-//         response = await routeAPINames(req, res)
-//     } catch (err) {
-//         console.log(err)
-//     }
-
-//     res.send(response)
-// })
 
 server.listen(port, function() {
     console.log("Listening on " + port)
