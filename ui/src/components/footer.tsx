3cc0255daaa863fddad51b74a36526a29f4c3b32
Ryan Small <rjsmall90@gmail.com>
2025-08-09 03:37:16 -0400

    last push to dev, feature branches subsequently

--- ui/src/components/footer.tsx
+++ ui/src/components/footer.tsx
@@ -8,10 +8,7 @@ export default function Footer(){
         <div>
             <hr className="h-px my-8 bg-[#F7F7F7] border-0 dark:bg-gray-700"></hr>
             <div className="grid auto-cols-3 grid-flow-col gap-8">
-                <div className="w-10"><Image fill={true} src={icon} alt="LinkedIn icon" /></div>
-                
-                {/* <Image fill={true} src="../assets/icons/287697_linkedin_icon.png" alt="LinkedIn icon" />
-                <Image fill={true} src="../assets/icons/287697_linkedin_icon.png" alt="LinkedIn icon" /> */}
+                <a href="https://linkedin.com/in/ryansmall90"><Image src={icon} alt="LinkedIn icon"  /></a>
             </div>
         </div>
     )
