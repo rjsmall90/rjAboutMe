0f96b09d12580b091150f31f69436b0644705749
Ryan Small <rjsmall90@gmail.com>
2025-08-10 23:01:30 -0400

    alert modal complete and footer updates

--- ui/src/components/footer.tsx
+++ ui/src/components/footer.tsx
@@ -1,19 +1,19 @@
 import Image from 'next/image'
 
 import igIcon from "../assets/icons/1161954_instagram_icon.png"
-import liIcon from "../assets/icons/287697_linkedin_icon.png"
-import fbIcon from "../assets/icons/5279111_network_fb_social media_facebook_facebook logo_icon.png"
+import liIcon from "../assets/icons/367594_linkedin_business_network_social_icon.png"
+import fbIcon from "../assets/icons/367582_facebook_social_icon.png"
 
 export default function Footer(){
     return (
         <div>
             <div className="inline-flex items-center justify-center w-full">
-                <hr className="w-270 h-1 my-8 bg-gray-200 border-1 rounded-sm dark:bg-gray-700"></hr>
-                <div className="absolute px-4 -translate-x-1/2 left-1/2 dark:bg-gray-900">
+                <hr className="w-270 h-0 my-8 bg-[#F7F7F7] border-1 rounded-sm "></hr>
+                <div className="absolute px-4 -translate-x-1/2 left-1/2">
                      <div className="flex flex-col sm:flex-row gap-6 overflow-x-auto sm:overflow-visible">
-                        <a href="https://www.instagram.com/rj_theengineer"><Image className="w-15 h-15 rounded-full object-scale-down mx-auto bg-[#F72585]" src={igIcon} alt="Instagram" /></a>
-                        <a href="https://linkedin.com/in/ryansmall90"><Image className="w-15 h-15 rounded-full object-scale-down mx-auto bg-[#F72585]" src={liIcon} alt="LinkedIn" /></a>
-                        <a href="https://www.facebook.com/ryan.small2"><Image className="w-15 h-15 rounded-full object-scale-down mx-auto bg-[#F72585]" src={fbIcon} alt="Facebook"/></a>
+                        <a href="https://www.instagram.com/rj_theengineer"><Image className="w-13 h-13 rounded-full object-scale-down mx-auto bg-[#F72585]" src={igIcon} alt="Instagram" /></a>
+                        <a href="https://linkedin.com/in/ryansmall90"><Image className="w-13 h-13 rounded-full object-scale-down mx-auto bg-[#F72585]" src={liIcon} alt="LinkedIn" /></a>
+                        <a href="https://www.facebook.com/ryan.small2"><Image className="w-13 h-13 rounded-full object-scale-down mx-auto bg-[#F72585]" src={fbIcon} alt="Facebook"/></a>
                     </div>
                 </div>
             </div> 
